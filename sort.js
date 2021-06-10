b86adbdbdee304f5c404cc28a1df8ec10d9a55b1
thorbo <jacobrothschild45@gmail.com>
2021-06-10 11:38:24 -0700

    basic searches added

--- sort.js
+++ sort.js
@@ -1,21 +1,25 @@
 // GLOBALS
-var TOTAL = 20          // number of elements to sort
+var TOTAL          // number of elements to sort
 const MIN = 1           // min number value
-const MAX = 20          // max number value
+const MAX = 50          // max number value
 const NUMSTART = [['position', 'value', {role: 'style'}]]       // header row for data in google charts
-const MAXSPEED = 2000   // slowest speed in ms
+const MAXSPEED = 1500   // slowest speed in ms
 var nums = []           // list of lists of ints [0,5,'green'],[1,17,'green'],[2,18,'green'],[3,12,'green'],[4, 9,'green']
 var SPEED
-var COUNTER = 0
+var searchnum
 
 // Load the Visualization API and the corechart package.
-google.charts.load('current', {'packages':['corechart']});
+google.charts.load('current', {'packages':['corechart']})
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Set TOTAL elements
     document.getElementById('number').value = 10
     TOTAL = document.getElementById('number').value
-    randNums();
 
+    // Load nums
+    randNums()
+
+    // Set SPEED
     document.getElementById('speed').max = MAXSPEED;
     document.getElementById('speed').value = MAXSPEED * 0.8;
     SPEED = MAXSPEED - (MAXSPEED*0.8);
@@ -23,16 +27,16 @@ document.addEventListener('DOMContentLoaded', () => {
     // Set a callback to run when the Google Visualization API is loaded.
     google.charts.setOnLoadCallback(drawChart);
 
+    // Set onclick values for all buttons
     document.querySelector('#randomize').onclick = () => {
-        randNums();
-        drawChart();
-    };
-
-    document.querySelector('#sort').onclick = numSort;
-    document.querySelector('#selection').onclick = selectionSort;
-    document.querySelector('#bubble').onclick = bubbleSort;
-    document.querySelector('#insertion').onclick = insertionSort;
-    document.querySelector('#random').onclick = randomSort;
+        randNums()
+        drawChart()
+    }
+    document.querySelector('#sort').onclick = numSort
+    document.querySelector('#selection').onclick = selectionSort
+    document.querySelector('#bubble').onclick = bubbleSort
+    document.querySelector('#insertion').onclick = insertionSort
+    document.querySelector('#random').onclick = randomSort
     document.querySelector('#merge').onclick = () => {
         // Copy nums by value into dup_nums. Perform sort on dup_nums
         const dup_nums = [...nums]
@@ -40,21 +44,20 @@ document.addEventListener('DOMContentLoaded', () => {
             dup_nums[i] = [...dup_nums[i]]
         }
         mergeSort(dup_nums)
-        };
-    document.querySelector('#shell').onclick = shellSort;
-    document.querySelector('#heap').onclick = heapSort;
-
-    document.querySelector('#quick').onclick = () => {quickSort(0, TOTAL - 1)};
-
-    document.querySelector('#speed').onchange = function () {SPEED = MAXSPEED - this.value};
-
-    document.querySelector('#number').onchange = function () {TOTAL = this.value};
-});
+    }
+    document.querySelector('#shell').onclick = shellSort
+    document.querySelector('#heap').onclick = heapSort
+    document.querySelector('#quick').onclick = () => {quickSort(0, TOTAL - 1)}
+    document.querySelector('#linear').onclick = () => {searchwrap(linear)}
+    document.querySelector('#binary').onclick = () => {searchwrap(binary)}
+    document.querySelector('#speed').onchange = function () {SPEED = MAXSPEED - this.value}
+    document.querySelector('#number').onchange = function () {TOTAL = this.value}
+})
 
 function drawChart() {
 
     // Create the data table.
-    var data = new google.visualization.arrayToDataTable(NUMSTART.concat(nums), false);
+    var data = new google.visualization.arrayToDataTable(NUMSTART.concat(nums), false)
 
     // Set chart options
     var options = {'title':'Numbers',
@@ -65,42 +68,40 @@ function drawChart() {
                    'legend' : 'none',
                    backgroundColor : 'none',
                    vaxis : {textPosition: 'none' },
-                   };
+                   }
 
     // Instantiate and draw our chart, passing in some options.
     var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-    chart.draw(data, options);
-};
+    chart.draw(data, options)
+}
 
 function randInt() {
     if (arguments.length == 0){
         return Math.floor(Math.random() * (MAX - MIN)) + MIN
     } else if (arguments.length == 2) {
         return Math.floor(Math.random() * (arguments[1] - arguments[0])) + arguments[0]
-    };
-};
+    }
+}
 
 function randNums() {
     // reset nums with random values
     nums = []
     for (i=0; i < TOTAL; i++) {
         nums.push([i, randInt(), '#069420']);
-        //'#bababa' grey
-        //'#069420' green
-    };
-};
+    }
+}
 
 function numSort() {
     nums.sort(function (a, b) {
         return a[1] - b[1]
-    });
+    })
 
     for (i=0; i < TOTAL; i++) {
         nums[i][0] = i
-    };
-    console.log(nums)
-    drawChart();
-};
+    }
+
+    drawChart()
+}
 
 async function selectionSort() {
     var smallVal
@@ -121,12 +122,12 @@ async function selectionSort() {
                 nums[j][2] = 'yellow'
                 if (prevIdx != i) {
                     nums[prevIdx][2] = '#069420'
-                };
+                }
                 prevIdx = j
                 drawChart();
                 await sleep(SPEED);
-            };
-        };
+            }
+        }
         tmp = nums[i][1]
         nums[i][1] = nums[prevIdx][1]
         nums[prevIdx][1] = tmp
@@ -136,8 +137,8 @@ async function selectionSort() {
         await sleep(SPEED);
         nums[i][2] = '#069420'
         nums[prevIdx][2] = '#069420'
-    };
-};
+    }
+}
 
 async function bubbleSort() {
     var tmp
@@ -204,64 +205,64 @@ async function insertionSort() {
 }
 
 async function quickSort(left, right) {
-    var l = left;
-    var r = right;
-    var tmp;
+    var l = left
+    var r = right
+    var tmp
     var color = '#'+ Math.floor(Math.random()*16777215).toString(16)    // random hex value
     // Set pivot index as middle of array (arbitrary)
     // Store pivot value
-    var pivot = Math.floor((l + r) / 2);
-    var pivotVal = nums[pivot][1];
+    var pivot = Math.floor((l + r) / 2)
+    var pivotVal = nums[pivot][1]
 
     // Base case: no modifications if there is only one value in array
     if (l >= r) {
         return 0;
-    };
+    }
 
     for (i=left; i<=right; i++) {
-        nums[i][2] = color;
-    };
+        nums[i][2] = color
+    }
 
-    nums[pivot][2] = 'red';
-    drawChart();
-    await sleep(SPEED);
+    nums[pivot][2] = 'red'
+    drawChart()
+    await sleep(SPEED)
 
     // Go until l and r pointers meet
     while (l <= r) {
         // Swap l if val(l) > pivotVal, keep going if not
         while (l < right && nums[l][1] < pivotVal) {
-            l++;
-        };
+            l++
+        }
         // Swap r if val(r) <>> pivotVal, keep going if not
         while (r > left && nums[r][1] > pivotVal) {
-            r--;
-        };
+            r--
+        }
         // Perform swap, checking if pointers are still valid
         if (l <= r) {
 
             if (l > pivot) {
                 nums[pivot][2] = color
-                pivot = l;
+                pivot = l
                 nums[pivot][2] = 'red'
             } else if (r < pivot) {
                 nums[pivot][2] = color
                 pivot = r
                 nums[pivot][2] = 'red'
-            };
+            }
 
-            tmp = nums[l][1];
-            nums[l][1] = nums[r][1];
-            nums[r][1] = tmp;
-            drawChart();
-            await sleep(SPEED);
-            l++;
-            r--;
-        };
-    };
+            tmp = nums[l][1]
+            nums[l][1] = nums[r][1]
+            nums[r][1] = tmp
+            drawChart()
+            await sleep(SPEED)
+            l++
+            r--
+        }
+    }
     nums[pivot][2] = color
-    quickSort(left, l-1);
-    quickSort(l, right);
-};
+    quickSort(left, l-1)
+    quickSort(l, right)
+}
 
 async function randomSort() {
     var sorted = false
@@ -404,12 +405,61 @@ async function shellSort() {
 
 function heapSort() {
     return 0
-};
+}
+
+function searchwrap(func) {
+    searchnum = Number(document.querySelector('#searchnum').value)
+    if (searchnum) {
+        func(searchnum)
+    } else {
+        alert("Enter Search Value")
+    }
+}
+
+async function linear(searchnum) {
+    for (i=0; i<nums.length; i++) {
+        if (i>0) {
+            nums[i-1][2] = '#069420'
+        }
+        nums[i][2] = 'yellow'
+        drawChart()
+        await sleep(SPEED)
+
+        if (nums[i][1] == searchnum) {
+            nums[i][2] = 'RoyalBlue'
+            drawChart()
+            break
+        }
+    }
+}
+
+async function binary(searchnum) {
+    var l = 0
+    var r = nums.length - 1
+
+    while (l < r) {
+        var m = Math.floor((l+r)/2 )
+        nums[m][2] = 'yellow'
+        drawChart()
+        await sleep(SPEED)
+
+        if (nums[m][1] == searchnum) {
+            nums[m][2] = "RoyalBlue"
+            drawChart()
+            break
+        } else if (nums[m][1] < searchnum) {
+            l = m + 1
+        } else {
+            r = m - 1
+        }
+        nums[m][2] = '#069420'
+    }
+}
 
 function sleep(ms) {
     // pulled from StackOverload. No built in sleep function in js
       return new Promise(resolve => setTimeout(resolve, ms))
-};
+}
 
 function bad_sleep(milliseconds) {
   const date = Date.now();
